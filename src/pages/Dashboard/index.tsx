22e54c00158de84ea7b429902f7f7e6c58a25aef
Nathália <nathaliamassis@gmail.com>
2020-10-19 16:08:26 -0300

    sign out

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useAuth } from '../../hooks/AuthContext';
+import Button from '../../components/Button';
 
 // import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
+
+  const { signOut } = useAuth();
+
   return (
     <View>
       <Text>
         Dashboard
       </Text>
+      <Button
+        onPress={() => signOut()}
+      >
+        Sair
+      </Button>
     </View>
   );
 }
