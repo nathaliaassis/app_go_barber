96ef68ba365778620e1646146400af55ea859723
nathaliaassis <nathaliamassis@gmail.com>
2021-03-30 13:44:59 -0300

    routes

---
+++ src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const AppointmentCreated: React.FC = () => {
+
+  return (
+    <View>
+      <Text>
+        AppointmentCreated
+      </Text>
+    </View>
+  );
+}
+
+export default AppointmentCreated;
---
+++ src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const CreateAppointment: React.FC = () => {
+
+  return (
+    <View>
+      <Text>
+        CreateAppointment
+      </Text>
+    </View>
+  );
+}
+
+export default CreateAppointment;
